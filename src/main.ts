c762b39c0a3f5767d7747abf5cdebdedcbfb23ce
abzhalimov-artem <abzalimov.artem.ed@gmail.com>
2026-01-18 23:41:43 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './router'
+import { Icon } from '@iconify/vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.component('Icon', Icon)
+
+app.use(router).mount('#app')
